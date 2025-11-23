4b8c048b17ad1176ca09126eaab630bba62bc7f7
zvakilov29 <zvakilov2018@gmail.com>
2025-11-23 12:54:04 +0400

    First Push Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Hello World ! ");
+    System.out.println("asldjkasd");
+    System.out.println("asdasd");
+    System.out.println("asdasd");
+}
